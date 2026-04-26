2d6ed7e395d0fc6b865468c0e3316620e3ecfa00
MinhThanh <tmpro285@gmail.com>
2026-04-26 07:48:25 +0700

    Rename tabs and update button texts
    Updated tab names and button descriptions for clarity.

--- GrinkHub.lua
+++ GrinkHub.lua
@@ -30,12 +30,13 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
       })
       
 ------ Tab
-     local Tab1o = MakeTab({Name = "Info"})
+     local Tab1o = MakeTab({Name = "Thông tin"})
      local Tab2o = MakeTab({Name = "Blox Fruit"})
      local Tab3o = MakeTab({Name = "Grow A Garden"})
      local Tab4o = MakeTab({Name = "99 Đêm"})
      local Tab5o = MakeTab({Name = "Xóa đồ họa fix lag"})
      local Tab6o = MakeTab({Name = "Stronget Battelround"})
+     local Tab7o = MakeTab({Namd= "Misc"})
      
 ------- BUTTON
     AddButton(Tab1o, {
@@ -49,7 +50,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
   end
   })
   AddButton(Tab1o, {
-     Name = "Mua key premium hoặc nhận key premium ở discord",
+     Name = "Mua key premium hoặc nhận key premium miễn phí ở discord",
     Callback = function()
   end
   })
@@ -101,12 +102,86 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/
 loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua", true))()
   end
   })
+  AddButton(Tab4o, {
+     Name = "FoxName",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bubu2k/Rubutv/refs/heads/main/Framkimcuong",true))()
+  end
+  })
   AddButton(Tab5o, {
      Name = "Duca fix lag",
    Callback = function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/minhducnek/DucaRoblox/refs/heads/main/FixLag.lua"))()
   end
   })
+  AddButton(Tab6o, {
+     Name = "Speed Hub",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+  end
+  })
+  AddButton(Tab6o, {
+     Name = "MinhNhat Lbs",
+    Callback = function()
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-MinhNhat-Tsb-62161"))()
+  end
+  })
+  AddButton(Tab6o, {
+     Name = "XY3RY",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xy3rxlol/xy3rxlol/refs/heads/main/loader",true))()
+  end
+  })
+  AddButton(Tab7o, {
+     Name = "FlyGui",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+  end
+  })
+  AddButton(Tab7o, {
+     Name = "Infinity Yield",
+    Callback = function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+  end
+  })
+  AddButton(Tab7o, {
+     Name = "Rejoin server",
+    Callback = function()
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+if #Players:GetPlayers() <= 1 then
+    -- Nếu chỉ có một mình, dùng phương thức chuyển server thông thường
+    TeleportService:Teleport(game.PlaceId, LocalPlayer)
+else
+    -- Vào lại đúng JobId của server hiện tại
+    TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, LocalPlayer)
+end
+  end
+  })
+  AddButton(Tab7o, {
+     Name = "Server hop",
+    Callback = function()
+local HttpService = game:GetService("HttpService")
+local TeleportService = game:GetService("TeleportService")
+local PlaceId = game.PlaceId
+
+local function serverHop()
+    local url = "https://roblox.com" .. PlaceId .. "/servers/Public?sortOrder=Desc&limit=100"
+    local response = HttpService:JSONDecode(game:HttpGet(url))
+    
+    for _, server in pairs(response.data) do
+        if server.playing < server.maxPlayers and server.id ~= game.JobId then
+            TeleportService:TeleportToPlaceInstance(PlaceId, server.id, game.Players.LocalPlayer)
+            break
+        end
+    end
+end
+
+serverHop()
+  end
+  })  })
   AddButton(Tab6o, {
      Name = "Speed Hub",
     Callback = function()
